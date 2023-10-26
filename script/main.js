15a7d70cc4d9e1faf12911358f201a1144d0ecf2
saddiq musa <saddiq0musa@gmail.com>
2023-10-26 23:24:17 +0300

    added headear massege

--- script/main.js
+++ script/main.js
@@ -1,9 +1,1 @@
-const arr1 = [9,5];
-const arr2 = [6,7] ;
 
-function mergeAndOrder(array1, array2) {
-    // write your code here
-    let result = [...array1,...array2];
-    return result.sort();
-}
-console.log(mergeAndOrder(arr1 , arr2));
